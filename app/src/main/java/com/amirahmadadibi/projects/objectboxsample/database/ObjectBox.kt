831a6af56f7838cef2b066954c41a2720b087675
amirahmadadibi <amirahmadadibi@gmail.com>
2019-11-10 11:36:45 +0330

    create objectBox Object

---
+++ app/src/main/java/com/amirahmadadibi/projects/objectboxsample/database/ObjectBox.kt
@@ -0,0 +1,18 @@
+package com.amirahmadadibi.projects.objectboxsample.database
+
+import android.content.Context
+import com.amirahmadadibi.projects.objectboxsample.model.MyObjectBox
+import io.objectbox.BoxStore
+
+/**
+ * Created by Amirahmad Adibi.
+ * ObjectBoxSample | Copyrights 2019-11-10.
+ */
+object ObjectBox {
+    lateinit var boxStore: BoxStore
+        private set
+
+    fun init(context: Context) {
+        boxStore = MyObjectBox.builder().androidContext(context.applicationContext).build()
+    }
+}
